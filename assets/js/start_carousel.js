96d16492c58003bea72f5b8ac2685ee40653dbab
Austin Feight <austin@chexology.com>
2018-07-16 01:38:29 -0400

    move new site in

---
+++ assets/js/start_carousel.js
@@ -0,0 +1,7 @@
+// $('.carousel').carousel({
+//   interval: 100
+// }).on('slide.bs.carousel', function (e) {
+//   console.log("hi");
+//   var nextH = $(e.relatedTarget).height();
+//   $(this).find('.active').parent().animate({ height: nextH }, 500);
+// });
